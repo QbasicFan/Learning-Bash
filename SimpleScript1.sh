6644a476a12188b4beedca004673bb1baac6922d
QbasicFan <philSerme@gmail.com>
2016-02-26 06:30:31 -0500

    Create SimpleScript1.sh

---
+++ SimpleScript1.sh
@@ -0,0 +1,27 @@
+
+#!/bin/bash
+function sep(){
+echo ------------------------------
+}
+
+#Do we clear the screen first or not ?
+
+if [ "$1" == "-c" ]
+then
+clear
+else
+echo MyIpCpHst [OTPION]
+echo -c   clear before display
+fi
+
+
+
+sep 
+echo  Locale \& Wireless lan0 
+ifconfig | grep 'inet addr'
+sep
+netstat -rn | awk ' { print $2 } ' 
+sep
+grep --color nameserver /etc/resolv.conf
+
+
